78d3bac69116438191aedd97589e4a61ff32da85
bigavilanezb00 <bigavilanezb00@elpuig.xeill.net>
2023-11-30 20:51:42 +0100

    Ejercicio tres sin terminar

--- apps/p1/src/components/Testimonio.js
+++ apps/p1/src/components/Testimonio.js
@@ -1,18 +1,20 @@
 import React from "react";
 import '../assets/Testimonio.css';
+import alexia from '../assets/img/alexia.jpeg'
 
 const Testimonio  = (props) => {
     return (
-        <div class="Testimonio">
-        <img/>
-        <header class="Testimonio-header">
-        <h2 >{props.nom} - {props.edad} anys</h2>
-        <p>{props.esport}</p>
-        <p>{props.institut}</p>
-        <p>{props.texte}</p>
-        </header>
-
-    </div>
+        <div className="testimonio">
+            <div className="testimonioA">
+            <img src={alexia} className="alexiaImg" alt="alexia"/>
+            </div>
+            <div className="testimonioB">
+                <h2 className="header">{props.nom} - {props.edad} anys</h2>
+                <p>{props.esport}</p>
+                <p>{props.institut}</p>
+                <p>{props.texte}</p>
+            </div>
+        </div>
     )
 };
 
